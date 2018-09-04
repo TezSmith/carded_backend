3ca2a9ee805237e55bd4648dbd723c85518124da
TezSmith <Montez.Smith1@gmail.com>
2018-09-04 18:41:45 -0400

    Connected Business Cards to Collection

--- app/controllers/api/v1/collections_controller.rb
+++ app/controllers/api/v1/collections_controller.rb
@@ -7,17 +7,28 @@ module Api
 			 render json: @collections
 			end
 
-			# create action for collections
 			def create
+	     bizcard = Bizcard.find_by(params[:card_name])
+
        @collection = Collection.new(collections_params)
+			 @collection.user.id = Collection.find_by(id: params["user"]["id"])
+			 @collection.bizcard = bizcard
 			 @collection.save
+			end
+
+			def show
+        @collection = Collection.find(params[:id])
 			end
 
 			private
 
 			def collections_params
-        params.require(:collections).permit(:collection_name, :user_id)
+        params.require(:collections).permit(:collection_name, :user)
 			end
+
+			# def bizcard_params
+      # 	params.require(:bizcard).permit(:card_name, :line1, :line2, :line3, :line4, :line5, :collection_name, :user_id)
+			# end
 
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
+      resources :users
        post '/login', to: 'users#login'
        post '/signup', to: 'users#create'
         resources :collections
-        resources :bizcards
-
+         resources :bizcards
     end #ends :v1
   end #ends :api
 
