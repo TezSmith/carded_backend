d23a4e93c45c4e1b8d7d23d0112f278b3ccabd7f
TezSmith <Montez.Smith1@gmail.com>
2018-09-06 18:00:30 -0400

    Basic Functionality Completed

--- app/serializers/bizcard_serializer.rb
+++ app/serializers/bizcard_serializer.rb
@@ -1,5 +1,5 @@
 class BizcardSerializer < ActiveModel::Serializer
-  attributes :id, :card_name, :collection_name, :line1, :line2, :line3, :line4, :line5
+  attributes :id, :card_name, :line1, :line2, :line3, :line4, :line5
 
   def collection_name
     object.collection.collection_name
