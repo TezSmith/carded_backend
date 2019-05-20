453b6cdb62c47df3e587893e0e932331837f372e
Tez Smith <montez.smith@flatironschool.com>
2019-05-20 10:46:53 -0400

    Testing json addition

--- config/application.rb
+++ config/application.rb
@@ -31,6 +31,6 @@ module CardedBackend
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-    config.autoload_paths << Rails.root.join('lib')
+    config.eager_load_paths << Rails.root.join('lib')
   end
 end
