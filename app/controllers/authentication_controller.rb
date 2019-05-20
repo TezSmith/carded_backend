77e541ca87d9d7c12bc39876ac8ac25520beae60
Tez Smith <montez.smith@flatironschool.com>
2019-05-20 18:05:08 -0400

    Added error messsages

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -3,9 +3,13 @@ class AuthenticationController < ApplicationController
     @user = User.find_by(username: params[:user][:username])
     if @user && @user.authenticate(params[:user][:password])
        token = JsonWebToken.encode({ user_id: @user.id })
-       render json: { jwt: token, username: @user.username, bizcards: @user.bizcards, collections: @user.collections }, status: :ok
+       if token.present?
+         render json: { jwt: token, username: @user.username, bizcards: @user.bizcards, collections: @user.collections }, status: :ok
+       else
+         render json: { message: 'Login failed. Please try again.' }
+       end
     else
-     render json: { error: 'Login Unsuccessful,'}, status: :unauthorized
+     render json: { message: 'Login Unsuccessful'}, status: :unauthorized
     end
  end
 
